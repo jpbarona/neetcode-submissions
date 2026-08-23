e4f997e01dbeb73d0b963350a5ad29e9a2c8d702
Juan Pablo Barona <141630180+jpbarona@users.noreply.github.com>
2026-08-23 18:55:11 -0400

    Add: design-hashset - submission-3

---
+++ Data Structures & Algorithms/design-hashset/submission-3.py
@@ -0,0 +1,39 @@
+N_BUCKETS = 103
+
+class MyHashSet:
+    def __init__(self):
+        self.buckets = [[] for _ in range(N_BUCKETS)] 
+       
+        def hash_function(x: int) -> int:
+            return x % N_BUCKETS
+        
+        self.h = hash_function
+        
+
+    def add(self, key: int) -> None:
+        bucket = self.buckets[self.h(key)]
+        exists = key in bucket
+        if exists:
+            return
+        
+        bucket.append(key)
+
+    def remove(self, key: int) -> None:
+        bucket = self.buckets[self.h(key)]
+        exists = key in bucket
+        if not exists:
+            return
+        
+        bucket.remove(key)
+        
+    def contains(self, key: int) -> bool:
+        bucket = self.buckets[self.h(key)]
+        return key in bucket
+        
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
