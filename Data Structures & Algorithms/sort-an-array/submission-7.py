08bf3a64304cf26fe5708b6f0c8bf0560e2de119
Juan Pablo Barona <141630180+jpbarona@users.noreply.github.com>
2026-08-24 16:21:54 -0400

    Add: sort-an-array - submission-7

---
+++ Data Structures & Algorithms/sort-an-array/submission-7.py
@@ -0,0 +1,33 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        #inplace mergesort
+        B = nums.copy()
+        def mergeSort(A,B,i,j):
+            if j-i==0:
+                return
+            if j-i==1:
+                B[i] = A[i]
+                return
+
+            k = (i+j)//2
+
+            mergeSort(B,A,i,k)
+            mergeSort(B,A,k,j)
+
+            l,r = i,k
+
+            for p in range(i,j):
+                if l<k and (r>=j or A[l]<A[r]):
+                    B[p]=A[l]
+                    l+=1
+                else:
+                    B[p]=A[r]
+                    r+=1
+
+        mergeSort(B,nums,0,len(nums))
+        return nums
+            
+
+            
+            
+        
